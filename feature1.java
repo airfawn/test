c72ff536d3eb3c69862cb0a58a7d9be60e26d4a2
airfawn737 <airfawn737@gmail.com>
2024-02-13 22:51:51 +0530

    feature

---
+++ feature1.java
@@ -0,0 +1,10 @@
+public class feature1 {
+    static int num; static String name;
+    feature1(String n, int a){
+        num = a;
+        name = n;
+    }
+    public static void main(String [] args){
+
+    }
+}
