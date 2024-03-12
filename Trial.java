643abd7c944e6ae01eaa8633847c0ed8707ff201
airfawn737 <airfawn737@gmail.com>
2024-03-12 12:56:26 +0530

    tried a diff method of sorting

--- Trial.java
+++ Trial.java
@@ -1,6 +1,15 @@
+import java.util.*;
 public class Trial {
     public static void main(String [] args)
     {
-        System.out.println("Testing out git");
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[10];
+        for(int i = 0;i<10;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int j = 0;j<10;j++){
+            System.out.println(arr[j]);
+        }
     }
 }
--- out/production/test_repo/readMe.md
+++ out/production/test_repo/readMe.md
@@ -1,1 +1,2 @@
-Hi
+Hi this is me testing out with git and its functions and utility 
+hope to make more projects!
